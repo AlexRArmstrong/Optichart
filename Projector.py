f5025b8bfa4a47664e930adf33ac3ae71122972d
Alexander Armstrong <alex@bigga.localdomain>
2014-04-10 01:50:28 -0400

    A class for each line in a chart.
    The line contains a series of sections (sometimes only 1) and
    additional data for describing the font, default character and
    spaceing.

--- Projector.py
+++ Projector.py
@@ -90,10 +90,105 @@ class Section(object):
 
 class Line(object):
 	'''
-	A record of each line in a chart file.
+	A record of each line in a chart file.  Each line consists of line sections,
+	with some extra data describing the line - the font, the default 
+	character for that line and the line spacing between this line and the next.
+	
+	Can be initilized with a list of sections, a default character position 
+	and a font name.  All are optional,	and if omitted the class will 
+	return an empty line.
 	'''
-	def __init__(self):
-		pass
+	def __init__(self, sections = [], def_chr = 0, font = None, line_space = '20/80'):
+		# Sections is a list of all the sections that make up a line.
+		self.sections = sections
+		
+		# The font for this line, if none should use default font.
+		self.font_name = font
+		
+		# The default character to use when centering. If none supplied, use the
+		# first character in the line.  This value the interger position of the
+		# letter in the line - count from 0.
+		self.default_character_position = def_chr
+		
+		# The distance between this and the next line.
+		self.line_spaceing = line_space
+		
+	def sections(self):
+		'''
+		Return the sections that make up the line.
+		'''
+		return self.sections
+		
+	def font(self):
+		'''
+		Return the font use for this line, if any.
+		'''
+		return self.font_name
+		
+	def defaultCharacterPosition(self):
+		'''
+		Return the position of the Default Character.  This is the position of
+		the character in the text of the line.  Start counting from 0.
+		'''
+		return self.default_character_position
+		
+	def lineSpaceing(self):
+		'''
+		Return the line spacing ratio.
+		'''
+		return self.line_spaceing
+		
+	def setFont(self, font):
+		'''
+		Set the font name for the line to font.  Return Ture on success, False
+		otherwise.
+		'''
+		try:
+			self.font_name = font
+			return True
+		except:
+			return False
+		
+	def setDefaultCharacter(self, position):
+		'''
+		Set the position of the Default Character.  Takes an interger value.
+		Returns True on succes, False otherwise.
+		'''
+		try:
+			self.default_character_position = position
+			return True
+		except:
+			return False
+	
+	def setLineSpacing(self, spacing):
+		'''
+		Sets the line spacing.  Is proper Snellen ratio. eg. 20/80.  Returns
+		True on success, False otherwise.
+		'''
+		try:
+			self.line_spaceing = spacing
+			return True
+		except:
+			return False
+	
+	def addSection(self, section, position = None):
+		'''
+		Add a line section - consisting of a ratio and text. Position is optional
+		and counts from 0.  If omitted the new section will be added to the end.
+		'''
+		# TODO: Decide on return value: True/False or the new list?
+		if position:
+			self.sections.insert(position, section)
+		else:
+			self.sections.append(section)
+			
+	def removeSection(self, section_position):
+		'''
+		Remove a ratio:text section at position section_position.
+		'''
+		del self.sections[section_position]
+		# TODO: Decide on return value: True/False or the new list?
+		
 		
 class Chart(object):
 	'''
