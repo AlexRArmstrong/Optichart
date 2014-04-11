eba89d8974f3a5ad39a3509e415b45619799bfd7
Alexander Armstrong <alex@bigga.localdomain>
2014-04-10 23:42:09 -0400

    A class modeling a chart.
    A chart consists of a series of lines and data describing page
    breaks.

--- Projector.py
+++ Projector.py
@@ -205,10 +205,142 @@ class Line(object):
 		
 class Chart(object):
 	'''
-	A model of a chart
+	A model of a chart.  Consists of a series of lines along with data
+	describing the page breaks and the chart file name.
 	'''
-	def __init__(self):
-		pass
+	def __init__(self, chart_name):
+		self.chart_name = chart_name
+		self.lines = []
+		self.pages = []
+		
+		file_handle = open(self.chart_name, 'r')
+		readChartFile(file_handle)
+		file_handle.close()
+	
+	def lines(self):
+		'''
+		Return the lines in the chart.
+		'''
+		return self.lines
+		
+	def addLine(self, line, pos = None):
+		'''
+		Adds a line to the list of lines, at optional position, pos.
+		'''
+		# TODO: Decide on return value: True/False or the new list?
+		if pos:
+			self.lines.insert(pos, line)
+		else:
+			self.lines.append(line)
+		
+	def removeLine(self, pos):
+		'''
+		Removes the line at position pos.
+		'''
+		# TODO: Decide on return value: True/False or the new list?
+		del self.lines[pos]
+	
+	def pages(self):
+		'''
+		Return the list of pages in the chart.
+		'''
+		return self.pages
+		
+	def addPage(self, pg):
+		'''
+		Appends a page to the number of pages. Returns True on success,
+		False otherwise.
+		'''
+		try:
+			self.pages.append(pg)
+			return True
+		except:
+			return False
+			
+	def removePage(self, pos):
+		'''
+		Removes a page at position pos.  Returns True on success,
+		False otherwise.
+		'''
+		try:
+			del self.pages[pos]
+			return Ture
+		except:
+			return False
+		# TODO: Is this a good way to do return values?
+		
+	def readChartFile(file_handle):
+		'''
+		Here we do the heavy lifting of parsing the chart file. Should only be
+		called from the constructor, and takes a file handle as argument.
+		'''
+		# Read the file into memory.
+		all_lines = file_handle.readlines()
+		# Now create an itteratible array so we know which line we're on.
+		line_numbers = range(len(all_lines))
+		# Create a line.
+		current_line = Line()
+		# Now read the lines.
+		for i in line_numbers:
+			# Ignore comments.
+			if all_lines[i].startswith('#'):
+				continue
+			# Ignore blank lines.
+			elif all_lines[i].isspace():
+				continue
+			# When we find ! set that line in the pages array.
+			elif all_lines[i].startswith('!'):
+				self.addPage(i)
+				continue
+			# If we find a Font, set the line font.
+			elif all_lines[i].upper().startswith('FONT'):
+				line = all_lines[i]
+				key, value = line.split('=')
+				current_line.setFont(value.strip())
+				continue
+			# If we find a Linesize spec., set line spacing.
+			elif all_lines[i].upper().startswith('LINESIZE'):
+				line = all_lines[i]
+				key, value = line.split('=')
+				current_line.setLineSpacing(value.strip())
+				continue
+			# If we have a data line...
+			elif all_lines[i].upper().startswith('20'):
+				line = all_lines[i]
+				# Check for multiple columns.
+				line_sections = line.split('|')
+				for each_section in line_sections:
+					ratio, text = each_line.split(':')
+					ratio = ratio.strip()
+					text = text.strip()
+					if text.startswith('"') or text.startswith("'"):
+						text = text.strip('"\'')
+					# Add a section to the current line.
+					a_section = Section(ratio, text)
+					current_line.addSection(a_section)
+					# Check for default character
+					default_chr_pos = 0
+					num_chrs = range(len(text))
+					for n in num_chrs:
+						if text[n] == '*':
+							default_chr_pos = n + 1
+							if default_chr_pos >= len(text):
+								default_chr_pos = len(text) - 1
+							current_line.setDefaultCharacter(default_chr_pos)
+							break
+						else:
+							current_line.setDefaultCharacter(default_chr_pos)
+							continue
+				# Done parsing line. Now add it to the chart.
+				# Note we do NOT creat a new line, as this would clear the spacing
+				# and font and those are only cleared if specified again or a new
+				# chart file is loaded.
+				self.addLine(current_line)
+				continue
+			else:
+				print "I don't know how to handle this line: \n#%i, %s" %(i, all_lines[i])
+				continue
+		return True	
 		
 class Projector(object):
 	'''
