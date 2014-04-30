ad24a41939c896b5bc256dc168e1620e3a7429c7
Alexander Armstrong <alex@bigga.localdomain>
2014-04-29 21:21:58 -0400

    Add missing imports and constants.

--- Slide.py
+++ Slide.py
@@ -26,6 +26,21 @@
 # functionLikeThis
 # variables_like_this
 
+import math
+import pygame
+
+from Chart import Chart
+
+# Define global constants.
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+
 class Slide(object):
 	'''
 	The text of a chart layed out and rendered.
