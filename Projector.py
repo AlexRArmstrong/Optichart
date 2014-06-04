0d14fe996624e37fcb58410fb01fefec2ca83ec9
Alexander Armstrong <alex@bigga.localdomain>
2014-06-03 21:24:40 -0400

    Create a variable for monitor resolution.
    Alows easier changing of screen size.

--- Projector.py
+++ Projector.py
@@ -113,7 +113,10 @@ class Projector(object):
 		# Create a screen.
 		# Calling set_mode with zeros will return a surface the same size as the
 		# current display.
-		self.screen = pygame.display.set_mode((0, 0), pygame.NOFRAME)
+	#	size = (0, 0)
+		size = (1920, 1080) # NOTE! The dpi depends on resolution and physical size if change one other must change too else get funny results.
+	#	size = (1280, 768)
+		self.screen = pygame.display.set_mode(size, pygame.NOFRAME)
 	#	screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 	#	screen = pygame.display.set_mode((XMAX, YMAX), pygame.FULLSCREEN)
 	#	screen = pygame.display.set_mode((XMAX, YMAX))
